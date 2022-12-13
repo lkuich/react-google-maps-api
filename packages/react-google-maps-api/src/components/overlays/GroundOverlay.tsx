2e69a27f6d916958d6e8028af236b445a49db483
Loren Kuich <loren.jk3@gmail.com>
2022-12-14 00:32:55 +0300

    Added GroundOverlayF

--- packages/react-google-maps-api/src/components/overlays/GroundOverlay.tsx
+++ packages/react-google-maps-api/src/components/overlays/GroundOverlay.tsx
@@ -1,4 +1,4 @@
-import { type ContextType, PureComponent } from 'react'
+import { type ContextType, PureComponent, useEffect, useContext, useMemo, memo } from 'react'
 
 import invariant from 'invariant'
 
@@ -27,18 +27,89 @@ export interface GroundOverlayProps {
   /** The opacity of the overlay, expressed as a number between 0 and 1. Optional. Defaults to 1. */
   opacity?: number | undefined
   /** This event is fired when the DOM dblclick event is fired on the GroundOverlay. */
-  onDblClick?: ((e: google.maps.MapMouseEvent) => void)  | undefined
+  onDblClick?: ((e: google.maps.MapMouseEvent) => void) | undefined
   /** This event is fired when the DOM click event is fired on the GroundOverlay. */
-  onClick?: ((e: google.maps.MapMouseEvent) => void)  | undefined
+  onClick?: ((e: google.maps.MapMouseEvent) => void) | undefined
   /** The url of the projected image */
   url: string
   /** The bounds that the image will be scaled to fit */
-  bounds: google.maps.LatLngBounds | google.maps.LatLngBoundsLiteral
+  bounds: any
   /** This callback is called when the groundOverlay instance has loaded. It is called with the groundOverlay instance. */
   onLoad?: ((groundOverlay: google.maps.GroundOverlay) => void) | undefined
   /** This callback is called when the component unmounts. It is called with the groundOverlay instance. */
   onUnmount?: ((groundOverlay: google.maps.GroundOverlay) => void) | undefined
+  debug?: boolean
+  visible?: boolean
 }
+
+function GroundOverlayFunctional({ url, bounds, options, visible, debug }: GroundOverlayProps) {
+  const map = useContext<google.maps.Map | null>(MapContext)
+
+  const imageBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(bounds.south, bounds.west),
+    new google.maps.LatLng(bounds.north, bounds.east)
+  );
+
+  const groundOverlay = useMemo(() => {
+    const overlay = new google.maps.GroundOverlay(url, imageBounds, {
+      ...options
+    });
+    return overlay
+  }, []);
+
+  useEffect(() => {
+    if (groundOverlay !== null) {
+      groundOverlay.setMap(map);
+
+      if (debug) {
+        console.log(map);
+        console.log(groundOverlay);
+      }
+    }
+  }, [map])
+
+  useEffect(() => {
+    if (typeof url !== 'undefined' && groundOverlay !== null) {
+      groundOverlay.set("url", url);
+      groundOverlay.setMap(map);
+
+      if (debug) {
+        console.log('Changing URL');
+      }
+    }
+  }, [groundOverlay, url]);
+
+  useEffect(() => {
+    if (typeof visible !== 'undefined' && groundOverlay !== null) {
+      groundOverlay.setOpacity(visible ? 1 : 0);
+
+      if (debug) {
+        console.log('Changing visibility');
+      }
+    }
+  }, [groundOverlay, visible]);
+
+  useEffect(() => {
+    const newBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(bounds.south, bounds.west),
+      new google.maps.LatLng(bounds.north, bounds.east)
+    );
+
+    if (typeof bounds !== 'undefined' && groundOverlay !== null) {
+      groundOverlay.set("bounds", newBounds);
+      groundOverlay.setMap(map);
+
+      if (debug) {
+        console.log('Changing bounds');
+        console.log(newBounds);
+      }
+    }
+  }, [groundOverlay, bounds])
+
+  return null;
+}
+
+export const GroundOverlayF = memo(GroundOverlayFunctional);
 
 export class GroundOverlay extends PureComponent<GroundOverlayProps, GroundOverlayState> {
   public static defaultProps = {
